ebb3411f4c2b14d76c188e5d2ac4b1e4302cd2f4
Hajoo Chung <hajoo.chung@gmail.com>
2021-03-10 16:23:34 +0000

    setup for typescript

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "hajoo", age = 24;
+const sayHi = (name, age) => {
+    console.log(`Hi, I'm ${name} and I'm ${age}`);
+};
+sayHi(name);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const name = "hajoo", age = 24
+
+const sayHi = (name, age?) => {
+console.log (`Hi, I'm ${name} and I'm ${age}`)
+}
+
+sayHi (name)
+
+export {};
